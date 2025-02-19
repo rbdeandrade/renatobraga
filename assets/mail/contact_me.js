6f238ec3933ba2d45d2fb633bc66bed26b3a92fa
Renato Braga de Andrade <rbdeandrade@gmail.com>
2025-02-19 00:04:25 +0000

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -21,49 +21,30 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "/assets/mail/contact_me.php",
+                url: "assets/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message,
+                    honeypot: "" // Honeypot field
                 },
                 cache: false,
                 success: function (response) {
-                    if (response === "200") {
-                        // Success message
-                        $("#success").html("<div class='alert alert-success'>");
-                        $("#success > .alert-success")
-                            .html(
-                                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                            )
-                            .append("</button>");
-                        $("#success > .alert-success").append(
-                            "<strong>Your message has been sent. </strong>"
-                        );
-                        $("#success > .alert-success").append("</div>");
-                        //clear all fields
-                        $("#contactForm").trigger("reset");
-                    } else {
-                        // Fail message
-                        $("#success").html("<div class='alert alert-danger'>");
-                        $("#success > .alert-danger")
-                            .html(
-                                "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
-                            )
-                            .append("</button>");
-                        $("#success > .alert-danger").append(
-                            $("<strong>").text(
-                                "Sorry " +
-                                    firstName +
-                                    ", it seems that my mail server is not responding. Please try again later!"
-                            )
-                        );
-                        $("#success > .alert-danger").append("</div>");
-                        //clear all fields
-                        $("#contactForm").trigger("reset");
-                    }
+                    // Success message
+                    $("#success").html("<div class='alert alert-success'>");
+                    $("#success > .alert-success")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-success").append(
+                        "<strong>Your message has been sent. </strong>"
+                    );
+                    $("#success > .alert-success").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
                 },
                 error: function () {
                     // Fail message
